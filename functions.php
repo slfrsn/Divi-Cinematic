90f2f62c056fd34f6271b453433dd6dc7f14eab0
Stef <sc@frsn.ca>
2015-09-09 09:27:03 -0700

    Updated functions.php
    Fixed automatic updates to check version properly and made it more
    portable for future themes or passers-by.

--- functions.php
+++ functions.php
@@ -81,22 +81,33 @@ function divi_cinematic_customize_register( $wp_customize ) {
 }
 
 // Automatic theme updates from the GitHub repository
-add_filter('pre_set_site_transient_update_themes', 'divi_cinematic_updates', 100, 1);
-function divi_cinematic_updates($data) {
-	$theme = wp_get_theme();
-	$repository = $theme->get('ThemeURI'); // We're using the 'Theme URI' property in our theme stylesheet header
-  $file = @json_decode(@file_get_contents('https://api.github.com/repos'.parse_url($repository)['path'].'/tags', false,
-      stream_context_create(['http' => ['header' => "User-Agent: scfrsn\r\n"]])
+add_filter('pre_set_site_transient_update_themes', 'automatic_GitHub_updates', 100, 1);
+function automatic_GitHub_updates($data) {
+  // Theme information
+  $theme   = get_stylesheet(); // Get the folder name of the current theme
+  $current = wp_get_theme()->get('Version'); // Get the version of the current theme
+  // GitHub information
+  $user = 'scfrsn'; // The GitHub username hosting the repository
+  $repo = 'divi-cinematic'; // Repository name as it appears in the URL
+  // Get the latest release tag from the repository. The User-Agent header must be sent, as per
+  // GitHub's API documentation: https://developer.github.com/v3/#user-agent-required
+  $file = @json_decode(@file_get_contents('https://api.github.com/repos/'.$user.'/'.$repo.'/releases/latest', false,
+      stream_context_create(['http' => ['header' => "User-Agent: ".$user."\r\n"]])
   ));
-	if($file) {
-		$data->response['divi-cinematic'] = array(
-			'theme'       => 'divi-cinematic',
-			// Strip the version number of any non-alpha characters (excluding the period)
-			'new_version' => filter_var(reset($file)->name, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION),
-			'url'         => $repository,
-			'package'     => $repository.'/archive/'.reset($file)->name.'.zip',
-		);
-	}
+  if($file) {
+    // Strip the version number of any non-alpha characters (excluding the period)
+    // This way you can still use tags like v1.1 or ver1.1 if desired
+    $update = filter_var($file->tag_name, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    // Only return a response if the new version number is higher than the current version
+    if($update > $current) {
+      $data->response[$theme] = array(
+          'theme'       => $theme,
+          'new_version' => $update,
+          'url'         => 'https://github.com/'.$user.'/'.$repo,
+          'package'     => $file->assets[0]->browser_download_url,
+      );
+    }
+  }
   return $data;
 }
 
